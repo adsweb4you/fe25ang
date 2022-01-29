902b835dcfad0c4437130c762f8c9dc9c3286162
adsweb4you <77510467+adsweb4you@users.noreply.github.com>
2022-01-29 21:58:48 +0400

    chns

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'fe25ang';
+ 
+  mydata= "ჩემი პირველი მონაცემი";
+  isOpen = false;
+  src = "../assets/img/img.png"
+  alt = "ფოტო ვერ დარენდერდა";
+  respImg = "myrespimg";
+  imgtype = true;
+
+  open = false;
+
+  updateDate(){
+    this.mydata = " განახლებულ მნიშვბელობას"
+    this.isOpen = true;
+    this.imgtype = !this.imgtype;
+  }
+
+show(){
+  this.open = true;
+}
+
 }
