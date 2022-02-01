5350f9a8dc79495cfb7b03d98a156f9b614dde6a
adsweb4you <77510467+adsweb4you@users.noreply.github.com>
2022-02-01 21:58:34 +0400

    last

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -7,36 +7,71 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostsComponent implements OnInit {
 
+  lang = 'fgdf'
+
    Posts = [
      {
        title:"post2",
        created_at:"25/03/2022",
        img:"assets/img/img.png",
        view:87,
+       slide:[
+        "assets/img/img1.jpg",
+        "assets/img/img2.png",
+        "assets/img/img3.png",
+        "assets/img/img4.png",
+       ]
      },
      {
       title:"post3",
       created_at:"25/03/2022",
       img:"assets/img/img.png",
       view:654,
+      slide:[
+        "assets/img/img1.jpg",
+        "assets/img/img2.png",
+        "assets/img/img3.png",
+        "assets/img/img4.png",
+       ]
     },
     {
       title:"post4",
       created_at:"25/03/2022",
       img:"assets/img/img.png",
       view:500,
+      slide:[
+        "assets/img/img1.jpg",
+        "assets/img/img2.png",
+        "assets/img/img3.png",
+        "assets/img/img4.png",
+       ]
     },
     {
       title:"post5",
       created_at:"25/03/2052",
       img:"assets/img/img.png",
       view:6897,
+      slide:[
+        "assets/img/img1.jpg",
+        "assets/img/img2.png",
+        "assets/img/img3.png",
+        "assets/img/img4.png",
+       ]
     },
    ]
 
   constructor() { }
 
   ngOnInit(): void {
+
+    this.Posts.forEach((el,index)=>{
+      console.log(el,index)
+    })
+  }
+
+
+  changimg(src:any, id:any){
+   this.Posts[id].img = src
   }
 
 }
